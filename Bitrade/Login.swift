85dca260e7e9ac16d21e7012723dc0bd7c10be0e
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2014-08-20 14:39:19 +0530

    Added an incomplete class to handle logging in

---
+++ Bitrade/Login.swift
@@ -0,0 +1,24 @@
+//
+//  Login.swift
+//  Bitrade
+//
+//  Created by Rukmal Weerawarana on 8/20/14.
+//  Copyright (c) 2014 Rukmal Weerawarana. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Login {
+    
+    // Class variables
+    var isLoggedIn: Bool
+    
+    //
+    // Initializer for the 'login' class
+    //
+    init() {
+        isLoggedIn = false
+    }
+    
+}
